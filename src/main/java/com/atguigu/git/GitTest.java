edd7ec04bca40e4ec3d442a36d3356848de9faa2
huahua <47920763@qq.com>
2022-04-25 23:47:38 +0800

    first hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("helloGit2");
+        System.out.println("hellohotfix");
     }
 }
